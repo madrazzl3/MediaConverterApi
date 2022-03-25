bddf2736abf38c2c0e7a3adc1dac2439108a9eec
Lazar Prijovic <madrazzl3@gmail.com>
2022-03-25 17:44:12 +0300

    Add random blob name generation

--- Controllers/Implementations/BlobMediaStorage.cs
+++ Controllers/Implementations/BlobMediaStorage.cs
@@ -17,7 +17,10 @@ namespace Stratis.MediaConverterApi
         public async Task<string> Store(string filePath, CancellationToken cancellationToken)
         {
             await BlobContainerClientInstance.CreateIfNotExistsAsync(PublicAccessType.Blob, null, null, cancellationToken);
-            var blobClient = BlobContainerClientInstance.GetBlobClient(Path.GetFileName(filePath));
+
+            var newFileName = Guid.NewGuid().ToString().Replace("-", "") + Path.GetExtension(filePath);
+            var blobClient = BlobContainerClientInstance.GetBlobClient(newFileName);
+
             await blobClient.UploadAsync(filePath, cancellationToken);
             return blobClient.Uri.ToString();
         }
